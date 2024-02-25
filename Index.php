9dcb8f41e7a18412c293419a92de3592fcdc3e69
Jahidul Islam <jahiduldev@gmail.com>
2024-02-25 13:15:20 +0600

    adafter design pattern code

--- Index.php
+++ Index.php
@@ -2,4 +2,6 @@
 //include 'Creational/factory.php';
 //include 'Creational/prototype.php';
 //include 'Creational/Singleton.php';
-include 'Creational/builder.php';
+//include 'Creational/builder.php';
+include 'Structural/adapter.php';
+//include 'Behavioral/builder.php';
