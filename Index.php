cc00b7ff9b75bb1cfb0e7efbd1818ff7b98c1445
Jahidul Islam <jahiduldev@gmail.com>
2024-03-13 13:18:58 +0600

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,5 +1,5 @@
 <?php
-include 'Creational/factory.php';
+//include 'Creational/factory.php';
 include 'Creational/prototype.php';
 include 'Creational/Singleton.php';
 include 'Creational/builder.php';
